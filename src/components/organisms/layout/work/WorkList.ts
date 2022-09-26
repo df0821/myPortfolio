c7ab89f134c99906a8558c1643727e1a91108aa2
user <d.fukushima21@gmail.com>
2022-09-27 00:06:12 +0900

    change text

--- src/components/organisms/layout/work/WorkList.ts
+++ src/components/organisms/layout/work/WorkList.ts
@@ -3,6 +3,6 @@ export const WorkList = [
     id: 0,
     imageSrc: require("../../../../assets/images/portfolio.jpg"),
     imageTitle: "porfolio",
-    text: "Reactを利用して作成しました。\nSkills:HTML5, CSS3, React, Typescript, Chakra UI\nOther:Atomic Design",
+    text: "Reactを利用して作成しました。\nSkills:HTML5, React, Typescript, Chakra UI\nOther:Atomic Design",
   },
 ];
